a4ca7e9dd55c3e9b33e5e28e9837b203246ec529
Hyunsu Kim 김현수 <129834692+khs220507@users.noreply.github.com>
2024-05-06 14:08:15 +0900

    part01

--- w1/src/main/java/org/zerock/w1/todo/TodoReadController.java
+++ w1/src/main/java/org/zerock/w1/todo/TodoReadController.java
@@ -4,20 +4,19 @@ import org.zerock.w1.todo.dto.TodoDTO;
 import org.zerock.w1.todo.service.TodoService;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "todoListController", urlPatterns = "/todo/list")
-public class TodoListController extends HttpServlet {
+public class TodoReadController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("/todo/list");
-        List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
-        req.setAttribute("list", dtoList);
-        req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+        System.out.println("/todo/read");
+        Long tno = Long.parseLong(req.getParameter("tno"));
+        TodoDTO dto = TodoService.INSTANCE.get(tno);
+        req.setAttribute("dto", dto);
+        req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
     }
 }
--- w1/src/main/webapp/WEB-INF/todo/read.jsp
+++ w1/src/main/webapp/WEB-INF/todo/read.jsp
@@ -1,9 +1,8 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: khs22
-  Date: 2024-04-29
-  Time: 오후 9:20
+  Date: 2024-05-06
+  Time: 오후 2:07
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -12,12 +11,10 @@
     <title>Title</title>
 </head>
 <body>
-<h1>List Page</h1>
-<ul>
-    <c:forEach var="dto" items="${list}">
-        <li>${dto}</li>
-    </c:forEach>
+<div>${dto.tno}</div>
+<div>${dto.title}</div>
+<div>${dto.dueDate}</div>
+<div>${dto.finished}</div>
 
-</ul>
 </body>
 </html>
